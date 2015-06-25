563dc77de4600e036ecc765bdc4e8680eb7d6adf
vijay33tube <kulavade.vijay@gmail.com>
2015-06-25 11:45:27 -0500

    First commit

---
+++ MVC4FormsAuth.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4FormsAuth.Web.Controllers
+{    
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Welcome!";
+
+            return View();
+        }
+    }
+}
